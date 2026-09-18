97b801bcb5bf843be020d3ca1d37d2b221e358ee
Ajo81 <130938378+Ajo81@users.noreply.github.com>
2026-09-18 13:41:50 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = "zummchat-v26";
+const CACHE = "zummchat-v29";
 const ASSETS = ["./", "./index.html", "./style.css", "./app3.js", "./matrix.js",
-                "./manifest.json", "./logo.png", "./icon.png"];
+                "./manifest.json", "./logo.png", "./icon.png", "./icon-192.png"];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
@@ -20,12 +20,14 @@ self.addEventListener("fetch", e => {
   const url = new URL(request.url);
   if (url.origin !== location.origin) return;
   e.respondWith(
-    caches.match(request).then(cached => cached || fetch(request)
+    fetch(request)
       .then(res => {
-        const copy = res.clone();
-        caches.open(CACHE).then(c => c.put(request, copy));
+        if (res && res.ok && (url.protocol === "http:" || url.protocol === "https:")) {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(request, copy));
+        }
         return res;
       })
-      .catch(() => caches.match("./index.html")))
+      .catch(() => caches.match(request).then(cached => cached || caches.match("./index.html")))
   );
 });

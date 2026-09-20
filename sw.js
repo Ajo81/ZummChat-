ef0a2c5b53a61337cf68edefd2408c6200b9ff79
Ajo81 <130938378+Ajo81@users.noreply.github.com>
2026-09-20 00:03:02 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // Año: 2026 - Todos los derechos reservados.
 // ============================================================
 
-const CACHE = "zummchat-v43";
+const CACHE = "zummchat-v44";
 const ASSETS = ["./", "./index.html", "./style.css", "./app3.js", "./matrix.js",
                 "./manifest.json", "./logo.png", "./icon.png"];
 
